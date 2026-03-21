545c875b2e6a1bfd792d820dce05fe91aaf3b6e2
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-03-21 12:30:26 +0100

    Remove Vaadin
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- src/main/java/com/thomasvitale/demo/ui/layout/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package com.thomasvitale.demo.ui.layout;
-
-import org.jspecify.annotations.NullMarked;
--- src/main/java/com/thomasvitale/demo/ui/views/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package com.thomasvitale.demo.ui.views;
-
-import org.jspecify.annotations.NullMarked;
